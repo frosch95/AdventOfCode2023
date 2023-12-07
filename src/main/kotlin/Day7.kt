efdefdb64ea4ed4ec5b558ba157e1ba3c93ec4c4
Andreas Billmann <andreas@billmann.de>
2023-12-08 00:09:35 +0100

    Day - refactored

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -2,83 +2,63 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main() {
-    val input = Files.readAllLines(Paths.get("src/main/resources/day7.txt"))
+    val input = Files.readAllLines(Paths.get("src/main/resources/example.txt"))
     println(puzzle1(input))
     println(puzzle2(input))
 }
 
 private fun puzzle1(input: List<String>): Int {
-    val hands = parseHands(input)
-
-    val sortedHands = hands.sortedWith(
-        comparator = {
-            hand1, hand2 ->
-            val strength1 = strengthOfHand(hand1)
-            val strength2 = strengthOfHand(hand2)
-            if (strength1 == strength2) {
-                hand1.cards.forEachIndexed { index, card ->
-                    if (strengthOfCard(card) > strengthOfCard(hand2.cards[index])) {
-                        return@sortedWith 1
-                    } else if (strengthOfCard(card) < strengthOfCard(hand2.cards[index])) {
-                        return@sortedWith -1
-                    }
-                }
-            }
-            strength1.compareTo(strength2)
-        }
-    )
-
-    val wins = sortedHands.mapIndexed { index, hand ->
-        val rank = index + 1
-        rank * hand.bid
-    }.sum()
-
-    return wins
+    return solve(input, ::strengthOfHand, ::strengthOfCard)
 }
 
 private fun puzzle2(input: List<String>): Int {
+    return solve(input, ::strengthOfHandWithJoker, ::strengthOfCardWithJoker)
+}
+
+private fun solve(input: List<String>,
+                  handStrength: (Hand) -> Int,
+                  cardStrength: (Char) -> Int): Int {
     val hands = parseHands(input)
 
     val sortedHands = hands.sortedWith(
             comparator = {
                 hand1, hand2 ->
-                val strength1 = strengthOfHandWithJoker(hand1)
-                val strength2 = strengthOfHandWithJoker(hand2)
+                val strength1 = handStrength(hand1)
+                val strength2 = handStrength(hand2)
+
                 if (strength1 == strength2) {
                     hand1.cards.forEachIndexed { index, card ->
-                        if (strengthOfCardWithJoker(card) > strengthOfCardWithJoker(hand2.cards[index])) {
-                            return@sortedWith 1
-                        } else if (strengthOfCardWithJoker(card) < strengthOfCardWithJoker(hand2.cards[index])) {
-                            return@sortedWith -1
-                        }
+                        val compared = cardStrength(card).compareTo(cardStrength(hand2.cards[index]))
+                        if (compared != 0)
+                            return@sortedWith compared
                     }
                 }
                 strength1.compareTo(strength2)
             }
     )
 
-    val wins = sortedHands.mapIndexed { index, hand ->
+    return calculateTotalWins(sortedHands)
+}
+
+private fun calculateTotalWins(sortedHands: List<Hand>): Int =
+    sortedHands.mapIndexed { index, hand ->
         val rank = index + 1
         rank * hand.bid
     }.sum()
 
-    return wins
-}
-
-private fun parseHands(input: List<String>) = input.map { it.split(" ") }
+private fun parseHands(input: List<String>) =
+    input.map { it.split(" ") }
         .map { Hand(it[0], it[1].toInt()) }
 
-private fun strengthOfHand(hand: Hand): Int {
-    val chars = countCards(hand)
-    return calculateStrength(chars)
-}
+private fun strengthOfHand(hand: Hand): Int =
+    calculateStrength(hand, ::countCards)
 
-private fun strengthOfHandWithJoker(hand: Hand): Int {
-    val chars = countCardsWithJoker(hand)
-    return calculateStrength(chars)
-}
+private fun strengthOfHandWithJoker(hand: Hand): Int =
+    calculateStrength(hand, ::countCardsWithJoker)
 
-private fun calculateStrength(chars: MutableMap<Char, Int>): Int {
+private fun calculateStrength(hand: Hand, cards: (Hand) -> Map<Char, Int>): Int {
+    val chars = cards(hand)
+
     if (chars.keys.size == 1) {
         return 7 // five of a kind
     }
@@ -105,48 +85,35 @@ private fun calculateStrength(chars: MutableMap<Char, Int>): Int {
     return 1 // high card
 }
 
-private fun countCards(hand: Hand): MutableMap<Char, Int> {
-    val chars = mutableMapOf<Char, Int>()
-    hand.cards.forEach {
-        if (chars.containsKey(it)) {
-            chars[it] = chars[it]!! + 1
+private fun countCards(hand: Hand): Map<Char, Int> =
+    hand.cards
+        .groupingBy { it }
+        .eachCount()
+
+
+private fun countCardsWithJoker(hand: Hand): Map<Char, Int> {
+    val chars = countCards(hand).toMutableMap()
+
+    if (chars.containsKey('J') && chars.keys.size > 1) {
+        val sortedMap = chars.toList().sortedByDescending { it.second }.toMap()
+        val highestChar = sortedMap.keys.first()
+
+        if (highestChar == 'J') {
+            // joker is highest
+            val secondChar = sortedMap.keys.elementAt(1)
+            chars[secondChar] = sortedMap[highestChar]!! + sortedMap[secondChar]!!
         } else {
-            chars[it] = 1
+            // joker is not the highest
+            chars[highestChar] = sortedMap[highestChar]!! + sortedMap['J']!!
         }
+        chars.remove('J')
     }
-    return chars
-}
-
-private fun countCardsWithJoker(hand: Hand): MutableMap<Char, Int> {
-
-    // if there is no joker, just count the cards
-    if (!hand.cards.contains('J'))
-        return countCards(hand)
-
-    val chars = countCards(hand)
-
-    if (chars.keys.size == 1) {
-        return chars // five of a kind
-    }
-
-    val sortedMap = chars.toList().sortedByDescending { it.second }.toMap()
-    val highestChar = sortedMap.keys.first()
-
-    if (highestChar == 'J') {
-        // joker is highest
-        val secondChar = sortedMap.keys.elementAt(1)
-        chars[secondChar] = sortedMap[highestChar]!! + sortedMap[secondChar]!!
-    } else {
-        // joker is not the highest
-        chars[highestChar] = sortedMap[highestChar]!! + sortedMap['J']!!
-    }
-    chars.remove('J')
 
     return chars
 }
 
-private fun strengthOfCard(card: Char): Int {
-   return when (card) {
+private fun strengthOfCard(card: Char): Int =
+    when (card) {
         'A' -> 14
         'K' -> 13
         'Q' -> 12
@@ -154,10 +121,9 @@ private fun strengthOfCard(card: Char): Int {
         'T' -> 10
         else -> card.toString().toInt()
     }
-}
 
-private fun strengthOfCardWithJoker(card: Char): Int {
-    return when (card) {
+private fun strengthOfCardWithJoker(card: Char): Int =
+    when (card) {
         'A' -> 14
         'K' -> 13
         'Q' -> 12
@@ -165,6 +131,5 @@ private fun strengthOfCardWithJoker(card: Char): Int {
         'T' -> 10
         else -> card.toString().toInt()
     }
-}
 
 private data class Hand(val cards: String, val bid: Int)
